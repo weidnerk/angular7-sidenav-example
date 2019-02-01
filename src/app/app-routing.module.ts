33faefe351439754301b3c3abb4072f21f59f381
Kevin Weidner <weidnerk@gmail.com>
2019-02-01 15:41:25 -0500

    initial load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const appRoutes: Routes = [
+  { path: '', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'first', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'second', component: SecondComponent, data: { title: 'Second Component' } }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
